71e5199649f34c582946955969f272f6ac7262b5
sonny <badtrip.parungao@gmail.com>
2020-05-31 20:07:10 +0800

    added comment on the return

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ use App\DateDifference;
 
 $dateDifference = new DateDifference('05-Feb-2020', '05-Mar-2020');
 echo $dateDifference->calculateDateDifference();
+//output is the total number of days, also if first date is greater than second date the return is zero.
